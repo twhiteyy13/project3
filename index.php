0e06812c91612556d5e79ace36387c77c170d3c2
Tiffany White <tiffanywhite@dhcp-10-5-27-235.wireless.rochester.edu>
2016-12-18 16:57:21 -0500

    Social Media images and content

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
 
             <div class="content">
 
-              <img src="Gardens.jpg" alt="Princes Street Gardens in Edinburgh, Scotland">
+              <img src="images/Gardens.jpg" alt="Princes Street Gardens in Edinburgh, Scotland">
  
 
             </div><!-- .content -->
@@ -45,17 +45,17 @@
 
         <div id="column1">
           <h2>Studio Art</h2>
-          <p><span class="nyc"></p>
+          <p>I'm a studio art major. I normally specialize in 3D art or installations because I like making art that people can touch and interact with by using all of their senses. Check out my <a href ="http://tiffanywhite12.wixsite.com/tiffany-art-design"> personal website </a> to see a collection of my work. </p>
         </div>
 
         <div id="column2">
           <h2>Business</h2>
-          <p> </p>
+          <p> I have a business minor with a concentration in marketing. I want to combine art and business to show the creative side of the business world. </p>
         </div>
 
         <div id="column3">
           
-          <img src="line-up.jpg" alt="Field Hockey Team">
+          <img src="images/line-up.jpg" alt="Field Hockey Team">
           
         </div>   
 
@@ -82,7 +82,27 @@
                   </div>
                   <div class="info">
                      <h2>Athletics</h2>
-                     <p></p>
+                     <p> 4x varsity starter and letter winner - Field Hockey 
+                      <ol>
+                        <li>Selection for the NFHCA National Academic Squad in ’13, ’14, ‘15 </li>
+                        <li>Selection for the Liberty League All Academic Team  in ’14, ‘15, '16 </li>
+                        <li>After the 2015 season, was selected to the Second Team All-Liberty League Team and Second Team NFHCA All-Region Team</li>
+
+                      </ol>
+
+                    </p>
+
+                    <br>
+
+                    <p> 2x varsity starter and letter winner - Softball
+
+                      <ol>
+                        <li>NFCA All-America Scholar Athlete team (2015)</li>
+                        <li>Second Team All- Liberty League Team as Outfielder </li>
+                        <li>Liberty League and UAA All-Academic Teams '15 </li>
+                      </ol>
+
+                     </p>
                   </div><!--.info-->
               </div>
 
@@ -93,7 +113,10 @@
                   </div>
                   <div class="info">
                      <h2>Edinburgh Capitals Ice Hockey</h2>
-                     <p></p>
+                     <p> In the spring semester of my junior year, I studied abroad in Edinburgh, Scotland, where I held the position of a sports marketing intern for the Edinburgh Capitals Elite Hockey Team. 
+                      I developed the idea for an inaugural "Caps' Student Night" and was respoonsible for managaging social media and marketing materials. 
+                      While the team is a professional level team, the organization also holds community outreach programs to develop the younger feeder teams. I design sponsor booklets, school program pamplets, and various hockey school and camp posters. My daily activities also included assisting my supervisors and other rink personnel with box office, raffle tickets, and fan interactions, merchandise, press releases and emails.
+                     </p>
                   </div><!--.info-->
               </div>
 
@@ -104,7 +127,12 @@
                   </div>
                   <div class="info">
                      <h2>Alpha Kappa Psi</h2>
-                     <p></p>
+                     <p>
+                      As a pledge, I was elected marketing chair and make all marketing materials associated to our fundraising efforts. 
+                      As a brother, I served one of the new member educator's this semester as well as webmaster. I will be serving as co-social chair for the upcoming semester. 
+
+
+                     </p>
                   </div><!--.info-->
               </div>
 
@@ -115,30 +143,16 @@
                   </div>
                   <div class="info">
                      <h2>Chi Omega</h2>
-                     <p></p>
+                     <p>
+
+
+                     </p>
                   </div><!--.info-->
               </div>
-
-              <!--Number 5-->
-               <div class="item">
-                  <div class="img">
-                    <img src="" alt="">
-                  </div>
-                  <div class="info">
-                     <h2>Employment</h2>
-                     <p></p>
-                  </div><!--.info-->
-              </div>
-
-
-
-
-
-
-
-
 
               </article>
+
+              <hr>
 
 
               <aside>
